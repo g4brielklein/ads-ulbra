da092524891cd1998f07bf9fdc623aa491adc514
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-03-13 08:24:00 -0300

    Programação WEB - Aula 04

---
+++ programacao-web/aula-04/introducao-ao-php.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Aula 4</title>
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	<body>
+		<h1>Aula 4</h1>
+		<h2>Introdução ao PHP</h2>
+		<h2>WampServer</h2>
+		<h2>Configuração do Servidor</h2>
+		<h2>Tipos de dados em PHP</h2>
+		<p>Verificar se o WampServer está instalado na máquina e os serviços ativos</p>
+		<h2>http://localhost</h2>
+		
+		<hr>
+		
+		<h2>Ler material de aula.</h2>
+		<h2>Assistir ao vídeo inicial</h2>
+	</body>
+</html>
---
+++ programacao-web/aula-04/pagina-teste.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Página teste</title>
+    </head>
+    <body>
+        <h1>Primeira Página</h1>
+        <?php
+            $texto = "Minha primeira página!";
+            echo $texto;
+        ?>
+    </body>
+</html>
