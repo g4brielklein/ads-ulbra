f033d9b4b7c349f9a33d11fcd1a2f1be6c978822
g4brielklein <gabrielkleinribas@gmail.com>
2020-04-11 04:11:44 -0300

    Laboratório de Programação - Aula 10 - AP1

--- laboratorio-de-programacao/aula-10-ap1/atividade-02.c
+++ laboratorio-de-programacao/aula-10-ap1/atividade-02.c
@@ -6,6 +6,7 @@ struct aluno {
 	char nome[30];
 	double g1, g2, media;
 	int qtd_presenca;
+	int aprovado; // 0 = Não(Reprovado) | 1 = Sim(Aprovado) 
 }turma[size];
 
 main() {
@@ -31,8 +32,16 @@ main() {
 		turma[i].media = (turma[i].g1 + turma[i].g2) / 2;
 	}
 	
+	for (i=0;i<size;i++) {
+		if (turma[i].media >= 6.0 && turma[i].qtd_presenca >= 51) {
+			turma[i].aprovado = 1;
+		} else {
+			turma[i].aprovado = 0;
+		}
+	}
+	
 	for(i=0;i<size;i++) {
-		printf("\nNome: %s\nNota da G1: %.1lf\nNota da G2: %.1lf\nQuantidade de presenças: %d\nMédia: %.1lf\n", 
-		turma[i].nome, turma[i].g1, turma[i].g2, turma[i].qtd_presenca, turma[i].media);
+		printf("\nNome: %s\nNota da G1: %.1lf\nNota da G2: %.1lf\nQuantidade de presenças: %d\nMédia: %.1lf\nAprovado: %d\n", 
+		turma[i].nome, turma[i].g1, turma[i].g2, turma[i].qtd_presenca, turma[i].media, turma[i].aprovado);
 	}
 }
