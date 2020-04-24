66809155ad07fc18af67f0fadb4a5adab68ebc37
g4brielklein <gabrielkleinribas@gmail.com>
2020-04-24 00:00:15 -0300

    Programação WEB - Aula 10

--- programacao-web/aula-10/banco.php
+++ programacao-web/aula-10/banco.php
@@ -1,24 +1,55 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+        <meta charset="UTF-8">
+        <title>Banco de Dados</title>
+        <style>
+            body {
+                background-color: #3b5998;
+                color: white;
+                padding: 10px;
+            }
 
-$servidor = '192.168.64.2';
-$user = 'root';
-$senha = '';
-$banco = 'ulbra';
-$TAB = '&nbsp;&nbsp;&nbsp;&nbsp;';
+            h1 {
+                font-size: 22px;
+            }
 
-$connect = mysqli_connect($servidor, $user, $senha);
-$db = mysqli_select_db($connect, $banco);
+            #banco {
+                border: 1px solid white;
+                padding: 10px;
+                width: 200px;
+            }
+        </style>
 
-$query_select = 'SELECT * FROM aluno';
-$select = mysql_query($connect, $query_select);
-$fetch = mysqli_fetch_row($select);
+        </style>
+    </body>
+        <h1>Dados do Banco</h1>
+        <div id="banco">
+            <?php
 
-echo 'CGU'.$TAB.'Nome <br/>';
+            $servidor = 'localhost';
+            $user = 'root';
+            $senha = '';
+            $banco = 'ulbra';
+            $TAB = '&nbsp;&nbsp;&nbsp;&nbsp;';
 
-echo $fetch[0].$TAB.$fetch[1].'<br/>';
+            $conect = mysqli_connect($servidor, $user, $senha);
+            $db = mysqli_select_db($conect, $banco);
 
-while ($fetch = mysqli_fetch_row($select)) {
-    echo $fetch[0].$TAB.$fetch[1].'<br/>';
-}
+            $query_select = 'SELECT * FROM aluno';
+            $select = mysqli_query($conect, $query_select);
+            $fetch = mysqli_fetch_row($select);
 
-?>
+            echo $TAB.$TAB.'CGU '.$TAB.$TAB.$TAB.'Nome <br/><br>';
+
+            echo $fetch[0].$TAB.$fetch[1].'<br/>';
+
+            while ($fetch = mysqli_fetch_row($select)) {
+                echo $fetch[0].$TAB.$fetch[1].'<br/>';
+            }
+
+            ?>
+        </div>
+    </body>
+</html>
