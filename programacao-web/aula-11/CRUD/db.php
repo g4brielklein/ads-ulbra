5d770af00921c3974e77d02b491d0d6dbf7b9f44
g4brielklein <gabrielkleinribas@gmail.com>
2020-05-01 03:35:14 -0300

    Atualizando os repositórios

---
+++ programacao-web/aula-11/CRUD/db.php
@@ -0,0 +1,8 @@
+<!-- Arquivo de configuração ou inclusão -->
+<?php
+    // Define as variáveis que serão utilizadas
+    $servidor = 'localhost';
+    $user = 'root';
+    $senha = '';
+    $banco = 'ulbra';
+?>
