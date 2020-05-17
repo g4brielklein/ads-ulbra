991307f0decf675344e84a00828422d08b2947f3
g4brielklein <gabrielkleinribas@gmail.com>
2020-05-16 23:57:09 -0300

    Update index.php and css folder

---
+++ programacao-web/aula-14/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Formulário</title>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <form method="POST" action="dados.php">
+            Nome: <input type="text">
+        </form>
+    </body>
+</html>
