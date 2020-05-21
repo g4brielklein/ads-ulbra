df63cdc0553810ceb2ffb2d0f39613e7986f095c
g4brielklein <gabrielkleinribas@gmail.com>
2020-05-21 11:54:39 -0300

    Prova AP2 Programação Web - Parte 1

--- programacao-web/aula-14/index.php
+++ programacao-web/aula-14/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html>
 
 <head>
     <meta charset="utf-8">
@@ -52,5 +52,4 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="assets/js/bootstrap.min.js"></script>
 </body>
-
 </html>
