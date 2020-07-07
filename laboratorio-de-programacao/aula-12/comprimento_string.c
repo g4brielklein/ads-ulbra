f2d3059bf149ee4d6eaa863d602178840db24b72
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-07-07 11:50:11 -0300

    Laboratório de Programação - Aula 12

--- laboratorio-de-programacao/aula-12/comprimento_string.c
+++ laboratorio-de-programacao/aula-12/comprimento_string.c
@@ -1,25 +1,31 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-int contar(char *str)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-    char frase[200];
-    int quantid;
-    
-    gets(&frase);
-    
-    quantid = contar(frase);
-    
-    printf("%d", quantid);
-    
-    return 0;
-    system("PAUSE");
-}
-int contar(char *str)
-{
+int contar(char *str) {
     int total=0;
     while( str[total] != '\0')
         total++;
     return total;
 }
+
+int main() {
+	char nome[50];
+	int comprimento, quantid; 
+	
+	
+	printf("Digite seu nome: ");
+	gets(&nome);
+	
+	// Retorna o comprimento da string utilizando a funcao strlen
+	printf("\nComprimento da string utilizando a funcao strlen: ");
+	comprimento = strlen(nome);
+	printf("%d", comprimento);
+	
+	// Retorna o comprimento da string NAO utilizando a funcao strlen
+	printf("\n\nComprimento da string NAO utilizando a funcao strlen: ");
+	quantid = contar(nome);
+	printf("%d", quantid);
+	return 0;
+}
+
