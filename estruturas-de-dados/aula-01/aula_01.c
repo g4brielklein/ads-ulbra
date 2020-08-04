45d530b418d18ec9cce52e4df21f6c3c2629e325
Gabriel Klein <gabrielkleinribas@gmail.com>
2020-08-03 21:36:20 -0300

    Estruturas de Dados - Aula 01

---
+++ estruturas-de-dados/aula-01/aula_01.c
@@ -0,0 +1,27 @@
+// Fazer todos os testes da AULA1
+
+// Listas encadeadas (LSE e LDE)
+// Manter a integridade da lista
+
+// Funcões para a lista funcionar (LSE ou LDE)
+// 1 - Incluir em qualquer parte da lista (inicio, meio ou fim)
+// 2 - Excluir em qualquer parte da lista (inicio, meio ou fim)
+// 3 - Listar os elementos
+// 4 - Alterar qualquer elemento
+// 5 - Procurar por um determinado elemento
+
+// *Qualquer operação precisa manter a integridade da lista
+
+// Funções 
+// Passagem de parâmtro por cópia e por refêrencia
+
+#include <stdio.h>
+
+int main() {
+    int vet[3];
+    int i;
+    for (i=0;i<3;i++) {
+        printf("\nVetor posicao %d -> ", i);
+        scanf("%d", &vet[i]);
+    }
+}
