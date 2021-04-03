7829d82862af00633e806153aba63ac53c1bd8a8
Gabriel Klein <gabrielkleinribas@gmail.com>
2021-04-03 18:34:17 -0300

    Removing classes

--- tecnologias-moveis/aula-05/classes/Construtor2.kt
+++
@@ -1,8 +0,0 @@
-package classes
-
-class Filme2(val nome: String, val anoLancamento: Int, val genero: String)
-
-fun main() {
-    val filme = Filme2 ("Monstros S.A.", 2001, "Comédia")
-    println("A ${filme.genero} ${filme.nome} foi lançada em ${filme.anoLancamento}")
-}
