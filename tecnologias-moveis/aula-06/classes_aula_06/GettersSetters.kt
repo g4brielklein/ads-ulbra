9f7727b224e94e75cc5c3f920be7a0720022ad64
Gabriel Klein <gabrielkleinribas@gmail.com>
2021-04-10 19:58:19 -0300

    Tecnologias Móveis - Aula 06

---
+++ tecnologias-moveis/aula-06/classes_aula_06/GettersSetters.kt
@@ -0,0 +1,30 @@
+package classes
+//personalizaar os getters e setters
+//estamos acessando os atributos sem usar o getters e setters
+
+class Cliente2{
+    constructor(nome: String){
+        this.nome = nome//cria o atributo depois do construtor
+    }
+
+    var nome: String
+        //field -> atributo corrente associado
+        get() = "Meu nome é ${field}" //variável field
+        set(value) {//set recebe um valor
+            //takeif -> lambda (coloca uma expressão true/false) que se for true o valor será retornado
+            // se for false retornará nulo
+            //use o ?: "elvis operator" para caso de ser vazio
+            field = value.takeIf { value.isNotEmpty() } ?:"Anônimo" //se estiver vazio
+        }
+}
+
+//não precisam ser implementados dezenas de métodos get e set como no Java
+//só interessa se precisar sobrescrever
+
+fun main() {
+    val c1 = Cliente2("")
+    println(c1.nome)
+
+    val c2 = Cliente2("Pedro")
+    println(c2.nome)
+}
