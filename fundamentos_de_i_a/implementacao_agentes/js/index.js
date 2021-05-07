c9718b08efe51f2d43af33a38050773f00e4af9c
Gabriel Klein <gabrielkleinribas@gmail.com>
2021-05-07 14:42:44 -0300

    Fundamentos de I.A - Implementação Agentes

--- fundamentos_de_i_a/implementacao_agentes/js/index.js
+++ fundamentos_de_i_a/implementacao_agentes/js/index.js
@@ -73,14 +73,16 @@ let celulas = [
         endereco: 3.0.toFixed(1)
     }
     
-] 
+]
 
-// gera veiculos randomicamente na matriz
-const vagas = [1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6];
+// gera veículos randomicamente na matriz
+const vagas = [1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6]
 let vagaRandomica1 = Math.floor(Math.random() * vagas.length)
 let vagaRandomica2 = Math.floor(Math.random() * vagas.length)
 let vagaRandomica3 = Math.floor(Math.random() * vagas.length)
 let vagaRandomica4 = Math.floor(Math.random() * vagas.length)
+let vagaRandomica5 = Math.floor(Math.random() * vagas.length)
+let vagaRandomica6 = Math.floor(Math.random() * vagas.length)
 
 let vaga1 = document.getElementById((vagas[vagaRandomica1]))
 vaga1.innerHTML = 'V'
@@ -98,7 +100,15 @@ let vaga4 = document.getElementById((vagas[vagaRandomica4]))
 vaga4.innerHTML = 'V'
 vaga4.pago = false
 
-// Percorre as céulas e marca os veiculos 
+let vaga5 = document.getElementById((vagas[vagaRandomica5]))
+vaga5.innerHTML = 'V'
+vaga5.pago = false
+
+let vaga6 = document.getElementById((vagas[vagaRandomica6]))
+vaga6.innerHTML = 'V'
+vaga6.pago = false
+
+// Percorre as céulas e marca os veículos
 
 for (let i = 0; i < celulas.length; i++) {
     
@@ -136,7 +146,6 @@ for (let j = 0; j < celulas.length; j++) {
     let cima = (document.getElementById(celulas[j].endereco).id - 1.0).toFixed(1)
     let atual = document.getElementById(celulas[j].endereco).id
 
-
     setTimeout(function(){ 
 
         // marca celula atual com F
@@ -154,98 +163,9 @@ for (let j = 0; j < celulas.length; j++) {
                 document.getElementById(deposito[1]).innerHTML = 'VI'
             }
         }
+    }, (j*1000) + 17850 );
 
-     }, (j*1000) + 17850 );
-
-     setTimeout(function(){ 
+    setTimeout(function(){ 
         document.getElementById(celulas[j].endereco).innerHTML = ''
-     }, (j*1050) + 17850 );
+    }, (j*1050) + 17850 );
 }
-
-
-
-
-
-// const analisaMatriz = () => {
-//     // pega a tabela pelo id
-//     let table = document.getElementById('matriz')
-
-//     let matriz = []
-
-//     // percorre celula / coluna
-//     for (var i = 0, row; row = table.rows[i]; i++) {
-//         let linha = []
-//         //iterate through rows
-//         //rows would be accessed using the "row" variable assigned in the for loop
-//         for (var j = 0, col; col = row.cells[j]; j++) {
-//             linha[col.id] = { pago: null }
-//             //   console.log(col.id,col.innerHTML)
-//             //   console.log(col.innerHTML)
-//         }
-//         matriz.push(linha)
-//     }
-//     return matriz[0]
-// }
-
-// const agenteFiscal = (posicao, matriz) => {
-//     const nomeAgente = 'F'
-
-//     let celula = matriz[posicao]
-//     let posicaoAtual = posicao
-
-//     for (let i = 0; i < 2; i++) {
-//         document.getElementById(posicaoAtual).innerHTML = 'F'
-        
-//         if (document.getElementById(posicaoAtual).innerHTML == 'V') { // veículo não verificado
-//             document.getElementById(posicaoAtual).innerHTML = 'VF'// marcar célula com 'VF'
-
-//             if (celula.pago === false) { // veículo irregular
-//                 console.log('veículo irregular')
-//                 document.getElementById(posicaoAtual).innerHTML = 'VIF'
-//                 //move
-//                 document.getElementById(posicaoAtual).innerHTML = 'VI'
-//             }
-//             else {
-//                 console.log('move') // funcao para mover o agente    
-//             }
-//         }
-//         else {
-//             console.log('move') // funcao para mover o agente    
-//         }
-
-//     }
-// }
-
-// document.getElementById('1.1').innerHTML = 'L'
-// // document.getElementById('2.0').innerHTML = 'F'
-
-// let matriz = analisaMatriz()
-
-// matriz['1.1'] = { pago: false }
-
-// agenteFiscal('2.0', matriz)
-
-// const moverAgenteFiscal = (posicao, matriz) => {
-//     cima = (parseFloat(posicao) - 1).toFixed(1)
-//     var posAtual = posicao
-
-//     console.log(document.getElementById(cima).innerHTML)
-
-//     for (let index = 0; index < 3; index++) {
-//         if (document.getElementById(cima).innerHTML === 'V') {
-//             console.log('veiculo')
-//             document.getElementById(cima).innerHTML = 'VF'
-//             document.getElementById(posAtual).innerHTML = ''
-//         } else {
-//             direita = (parseFloat(posAtual) + 0.1).toFixed(1)
-//             setTimeout(function(){ document.getElementById(direita).innerHTML = 'F' }, 1000);
-//             // setTimeout(document.getElementById(direita).innerHTML = 'F', 1000)
-//             cima = (parseFloat(direita) - 1).toFixed(1)
-//             setTimeout(function(){ document.getElementById(posAtual).innerHTML = '' }, 2000);
-            
-//             posAtual = direita
-//         }
-//     }
-// }
-
-// console.log(moverAgenteFiscal('2.0', matriz))
