17f6d2f0fabda0828bd48217cbf4849c7662bb4f
Gabriel Klein <gabrielkleinribas@gmail.com>
2021-05-13 22:29:30 -0300

    Fundamentos de Inteligência Artificial - Implementação dos agentes

--- fundamentos_de_i_a/implementacao_agentes/js/index.js
+++ fundamentos_de_i_a/implementacao_agentes/js/index.js
@@ -12,7 +12,7 @@ let vaga1 = document.getElementById((vagas[vagaRandomica1]))
 vaga1.innerHTML = 'V'
 vaga1.pago = true
 
-let vaga2 = document.getElementById((vagas[vagaRandomica2]))
+let vaga2 = document.getElementById(1.3)
 vaga2.innerHTML = 'V'
 vaga2.pago = false
 
@@ -22,7 +22,7 @@ vaga3.pago = true
 
 let vaga4 = document.getElementById((vagas[vagaRandomica4]))
 vaga4.innerHTML = 'V'
-vaga4.pago = false
+vaga4.pago = true
 
 // Percorre as céulas e marca os veículos
 for (let i = 0; i < celulas.length; i++) {
@@ -54,34 +54,65 @@ for (let i = 0; i < celulas.length; i++) {
      }, i*1050);
 }
 
+let ok = 0;
+
 for (let j = 0; j < celulas.length; j++) {
-    
-    let atual = (document.getElementById(celulas[j])).id
-    let acima = ((document.getElementById(celulas[j])).id - 1).toFixed(1)
 
-    setTimeout(function(){ 
+    // if (ok == 1 || j == 1) {
 
-        // marca celula atual com F
-        document.getElementById(atual).innerHTML = 'G'
+        let atual = (document.getElementById(celulas[j])).id
+        let acima = ((document.getElementById(celulas[j])).id - 1).toFixed(1)
 
-        // Verifica se a celula de cima é 'VI'
-        if (document.getElementById(acima).innerHTML == 'VI') {
-            document.getElementById(acima).innerHTML = 'L'
-            document.getElementById(atual).innerHTML = 'GVI'
-            for (x = j; x < celulas.length; x++) {
-                let atualX = (document.getElementById(celulas[x])).id
-                let acimaX = ((document.getElementById(celulas[x])).id - 1).toFixed(1)
-                
-                setTimeout(function(){ 
-                
-                    document.getElementById(atualX).innerHTML = 'GVI'
+        setTimeout(function(){ 
 
-                }, (x*1000) + (j*1000) + 17850 );
+            // marca celula atual com F
+            if (document.getElementById(acima).id > '1.3') {
+                document.getElementById(atual).innerHTML = ''
+            } else {
+                document.getElementById(atual).innerHTML = 'G'
             }
-        }
-    }, (j*1000) + 17850 );
 
-    setTimeout(function(){ 
-        document.getElementById(atual).innerHTML = ''
-    }, (j*1050) + 17850 );
+        
+            // Verifica se a celula de cima é 'VI'
+            if (document.getElementById(acima).innerHTML == 'VI') {
+                document.getElementById(acima).innerHTML = 'L'
+                document.getElementById(atual).innerHTML = 'GVI'
+                for (x = j; x < celulas.length; x++) {
+                    // document.getElementById(atual).innerHTML = ''
+                    let atualX = (document.getElementById(celulas[x])).id
+                    let acimaX = (document.getElementById(celulas[x]).id - 1).toFixed(1)
+
+                    setTimeout(function(){ 
+                        
+                        document.getElementById(atualX).innerHTML = 'GVI'
+                        if (document.getElementById(acimaX).id == '3.1') {
+                            document.getElementById(5.1).innerHTML = 'VI'
+                            document.getElementById(atualX).innerHTML = 'G'
+                        }
+
+                        if ((document.getElementById(atualX).id == '4.0')
+                            || (document.getElementById(atualX).id == '3.0')) {
+                            document.getElementById(atualX).innerHTML = 'G'
+                        }
+
+                        // if (document.getElementById(5.1).innerHTML == 'VI') {
+                        //     ok = 1
+                        // }
+                        
+                    }, (x*1000));
+
+                    setTimeout(function(){ 
+                        document.getElementById(atualX).innerHTML = ''
+                    }, (x*1050));
+                }
+                
+            }
+            
+            
+        }, (j*1000) + 17850 );
+
+        setTimeout(function(){ 
+            document.getElementById(atual).innerHTML = ''
+        }, (j*1050) + 17850 );
+    // }
 }
