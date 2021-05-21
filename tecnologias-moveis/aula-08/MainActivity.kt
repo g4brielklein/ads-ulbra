fd7885c7977a1ee6249644b56db4728e445ffc19
g4brielklein <gabrielkleinribas@gmail.com>
2021-05-20 22:31:15 -0300

    Tecnologias Móveis - Aula 08

--- tecnologias-moveis/aula-08/MainActivity.kt
+++ tecnologias-moveis/aula-08/MainActivity.kt
@@ -6,6 +6,25 @@ import android.os.Bundle
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //definindo o arquivo de layout
+        setContentView(R.layout.activity_main)
+
+        //acessando o spinner
+        val spn_sexo = findViewById<Spinner>(R.id.spn_sexo)
+
+        //acessando a caixa de idade
+        val txt_idade = findViewById<EditText>(R.id.txt_idade)
+
+        //acessando o botão de calcular
+        val btn_calcular = findViewById<Button>(R.id.btn_calcular)
+
+        //acessando o texto de resultado
+        val txt_resultado = findViewById<TextView>(R.id.txt_resultado)
+
+        spn_sexo.adapter = ArrayAdapter<String>(
+            this, android.R.layout.simple_spinner_dropdown_item,
+            listOf("masculino", "feminino")
+        )
     }
 }
 
